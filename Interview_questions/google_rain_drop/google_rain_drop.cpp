98f80e6821c47132efadafe2608e01b3e43bc183
Yuyin Sun <cherubjywh@gmail.com>
2015-11-03 23:19:06 -0800

    google rain drop

---
+++ Interview_questions/google_rain_drop/google_rain_drop.cpp
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  google_rain_drop
+//
+//  Created by Yuyin Sun on 15-11-3.
+//  Copyright (c) 2015年 Yuyin Sun. All rights reserved.
+//
+
+#include <iostream>
+
+/*
+ I don't have time to implement the problem today. I will just write down the problem roughly, and my idea of solving the problem
+ 
+ A hallway: -----------------
+ Uniform rain drop, each rain drop will wet x in length
+ Decide when the whole hallway is wet
+ 
+ I got this interview question from an undergrad who interviewed with Google
+ 
+ */
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
