ee4045395093d23d40756887d5d03b2a5cae70e3
Yuyin Sun <sunyuyin@cs.washington.edu>
2015-11-06 14:34:22 -0800

    add more pitfall examples

--- Random_cpp/pitfalls.cpp
+++ Random_cpp/pitfalls.cpp
@@ -27,16 +27,32 @@ class Array {
 		int size;
 };
 
+template<typename T>
+class Brray {
+	public:
+		explicit Brray(int s):size(s){cout << "in int constructor\n";}
+		int size;
+};
+
 void pitfall_constructor_2() {
 	Array<int> a(32);
 	// Constructors with one argument sever double duty as type conversions
 	Array<int> b = 32;
 	Array<int> c = 32.0;
 	// The previous two examples coulde explain what is double duty in this case
+	// Lesson: Avoid constructors with a single integer argument! Use the explicit keyword if you cannot avoid them
 
 	cout << "a: " << a.size << endl;
 	cout << "b: " << b.size << endl;
 	cout << "c: " << c.size << endl;
+
+	/*
+
+	// Try to call as converting constructor, but it does not work this case because of explict keyword
+	 Brray<int> d = 32;
+	 */
+
+	Brray<int> e(32);
 }
 
 int main(int argc, char* argv[]) {
