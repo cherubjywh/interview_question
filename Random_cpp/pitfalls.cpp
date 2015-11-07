729865a8db0afffa017f8d099e0fa82f1125160e
Yuyin Sun <cherubjywh@gmail.com>
2015-11-07 10:01:06 -0800

    more pitfalls

--- Random_cpp/pitfalls.cpp
+++ Random_cpp/pitfalls.cpp
@@ -7,6 +7,7 @@ void pitfall_constructor_1() {
 	// A declaration of a function
 	// The stupid thing is useless at all
 	// It is called "declaring a function in a local scope"
+	// It is interprated as function declaration
 	string b();
 	string c;
 
@@ -55,9 +56,65 @@ void pitfall_constructor_2() {
 	Brray<int> e(32);
 }
 
+void pitfall_new_1() {
+	// Moral: Array/pointer duality is dumb, but unfortunately pervasive in C and C++
+	int * p = new int(10);
+	cout << "Pitfall_new_1: " << *p << endl;
+	delete p;
+}
+
+/*
+
+   // Even compiler is checking and telling me that _capacity is used before it is initialized
+   // Sweet that this type of error can be detected nowdays
+
+template<typename T>
+class A_init_members {
+	public:
+		// This is tricky, initialization follows the member declaration order, not the initializer order
+		A_init_members(int size):
+			_size(size),_capacity(size + 10),_data(new T[_capacity])
+		{}
+
+		T operator[] (size_t i) {
+			return _data[i];
+		}
+
+	private:
+
+		T *_data;
+		int _size;
+		int _capacity;
+};
+*/
+
+template<typename T>
+class A_init_members_correct {
+	public:
+		// Tip: Do not use data member in initializer expressions
+		A_init_members_correct(int size):
+			_size(size), _capacity(size + 10), _data(new T[size + 10])
+	{}
+	private:
+
+		T *_data;
+		int _size;
+		int _capacity;
+};
+
+void pitfall_constructor_initialize_members() {
+//	A_init_members<int> A(10);
+	// Should not be OK
+//	cout << A[9] << endl;
+	
+	A_init_members_correct<int> A_correct(10);
+}
+
 int main(int argc, char* argv[]) {
 
 	// pitfall_constructor_1();
-	pitfall_constructor_2();
+	// pitfall_constructor_2();
+	// pitfall_new_1();
+	pitfall_constructor_initialize_members();
 
 }
